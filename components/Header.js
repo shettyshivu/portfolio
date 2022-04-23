3e78f8513cb18fe526d4059b114904d7085b4c52
Shivaprasad shetty <shivaprasadshetty555@gmail.com>
2022-04-23 12:55:07 +0530

    header page modified

--- components/Header.js
+++ components/Header.js
@@ -2,23 +2,26 @@ import React from "react";
 
 function Header() {
   return (
-    <>
+    <div className="position-relative h-100 header-main-container">
       <div className="header">
-        <div className="header-content">
-          <div class="header-greeting">
+        <div className="header-content" data-aos="zoom-out">
+          <div className="header-greeting">
             <h2>&lt;Hello&nbsp;&#47;&gt;</h2>
           </div>
-          <div class="header-name">I'm Shivaprasad Shetty</div>
-          <div class="header-desciption">Full Stack Web Developer</div>
+          <div className="header-name">I&apos;m Shivaprasad Shetty</div>
+          <div className="header-desciption">
+            <span>Full Stack</span> Developer
+          </div>
           <div className="lets-talk-button text-center">
-              <button>Let's Talk!</button>
+            <button>Let&apos;s Talk!</button>
           </div>
         </div>
-        <div className="header-image">
-            <img src="../hero-girl.png" alt="" className="img-fluid" />
+        <div className="header-image" data-aos="zoom-out" data-aos-delay="300">
+          <img src="../hero-girl.png" alt="" className="img-fluid" />
         </div>
       </div>
-    </>
+      <div className="wave"></div>
+    </div>
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import "../styles/style.css";
-
+import "aos/dist/aos.css";
+import AOS from "aos";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  }, []);
   return <Component {...pageProps} />;
 }
 
