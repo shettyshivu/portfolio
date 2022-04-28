b4e9e286f8843d6e5b197efca8adfed7f804d538
Shivaprasad shetty <shivaprasadshetty555@gmail.com>
2022-04-28 12:41:58 +0530

    favicon added

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Head>
         <title>Shivaprasad Shetty</title>
         <meta name="description" content="Shivaprasad's Portfolio Website" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.png" />
       </Head>
       <Navbar />
       <Header />
