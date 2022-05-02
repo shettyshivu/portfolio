6820aa425b9a15bce88b221018a9b9505bb97f59
Shivaprasad shetty <shivaprasadshetty555@gmail.com>
2022-05-02 11:07:18 +0530

    input width chnaged

--- components/About.js
+++ components/About.js
@@ -145,7 +145,7 @@ const About = () => {
                             aria-valuenow="60"
                             aria-valuemin="0"
                             aria-valuemax="100"
-                            style={{ width: "60%" }}
+                            style={{ width: "70%" }}
                           ></div>
                         </div>
                       </div>
